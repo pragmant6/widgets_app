4c522dfee09563c2e9dfa4a43e06bd2f7963bc54
Miguel Angel Prado Garcia <mprado@mosbit.com>
2025-03-04 10:36:29 -0600

    se implemento HomeScreen

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Flutter Examples')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Examples')),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final MenuItem item = appMenuItems[index];
+        return _CustomListTitle(item: item);
+      },
+    );
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+  const _CustomListTitle({required this.item});
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(item.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      title: Text(item.title),
+      onTap: () {
+        Navigator.pushNamed(context, item.link);
+      },
+    );
   }
 }
